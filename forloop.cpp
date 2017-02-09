615c6270510f6e22248e5484299182a179381605
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-09 17:25:40 +0500

    Create forloop.cpp

---
+++ forloop.cpp
@@ -0,0 +1,19 @@
+// forloop.cpp
+// demonstrates simple FOR loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int j;   //gefine a loop variable.
+
+for (j=0 ; j<15 ; j++)
+cout<< j* j << "   ";
+   // display the square of j
+cout<< endl;   
+
+return 0;
+
+
+}
